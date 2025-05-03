e1bf0db48d079b6864f75d977ef08e205026d6f6
salmanfaraj06 <salman.faraj06@gmail.com>
2025-05-03 14:23:52 +0530

    feat: Add WhatDrivesUs component with responsive design and animations

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,12 @@
 import CTASection from "@/components/CTASection";
 import HeroBanner from "@/components/HeroBanner";
+import WhatDrivesUs from "@/components/about/WhatDrivesUs"; // Import the new component
 
 export default function AboutPage() {
   return (
     <div className="mx-4 pt-24">
       <HeroBanner video="/test.mp4" description="Driving the Next Wave of Student Engagement and Enrollment Through AI Innovation" />
+      <WhatDrivesUs /> {/* Add the WhatDrivesUs section */}
       <CTASection
       title="Discover the Ideas, Innovations, and Insights Shaping the Future of Education and Student Engagement"
       description="From AI breakthroughs to transformative strategies in higher ed, our blog keeps you informed, inspired, and ahead of the curve."

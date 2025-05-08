50b67736ca5c4b8b0ac14a9719fdf1c3ceb60660
hkallk <hashiimkalam@gmail.com>
2025-05-08 20:02:34 +0530

    style(Header): adjust header width and change border radius for improved aesthetics

--- src/data/header.ts
+++ src/data/header.ts
@@ -2,6 +2,6 @@ export const HEADER = [
   { title: "Features", href: "/features" },
   { title: "About", href: "/about" },
   { title: "Blog", href: "/blog" },
-  { title: "Careers", href: "/careers" },
+  // { title: "Careers", href: "/careers" },
   { title: "Contact Us", href: "/contact" },
 ];

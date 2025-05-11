0c12739b51cf583ceb36ced40ab24fb6f1423aaf
salmanfaraj06 <salman.faraj06@gmail.com>
2025-05-11 15:00:05 +0530

    fix: adjust padding in CompanyOverview section for improved layout

--- src/components/home/company-overview.tsx
+++ src/components/home/company-overview.tsx
@@ -2,7 +2,7 @@ import RippleButton from "../RippleButton";
 
 export default function CompanyOverview() {
   return (
-    <div className="md:mx-12 lg:mx-24 py-12 lg:py-24 mt-12">
+    <div className="md:mx-12 lg:mx-24 py-12 lg:py-16 mt-12">
       <p className="text-[32px] lg:text-[45px] text-lato font-[700] lg:leading-[60px] mb-6 lg:w-[80%]">
         Edusight delivers AI-powered tools that enhance engagement, simplify
         operations, and support student success.
